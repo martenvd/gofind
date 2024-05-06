4e6bd7dd6f5617f4d951cc3601c3ae3cc14f21e4
martenvd <vandijk.marten@gmail.com>
2024-05-07 00:17:07 +0200

    caching

--- main.go
+++ main.go
@@ -2,7 +2,10 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,7 +16,80 @@ import (
 )
 
 func main() {
+
+	updateCache := flag.Bool("u", false, "Whether or not to update the gofind cache.")
+	flag.Parse()
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *updateCache || !fileExists(homeDir+"/.gofind/dirs.txt") {
+		dirs, err := walkPaths()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cacheDirs(homeDir, dirs)
+	}
+
+	dirs, err := readDirs(homeDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prompt(dirs)
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func isFlag() bool {
+	flags := []string{"-u"}
+
+	for _, flag := range flags {
+		if os.Args[1] == flag {
+			return true
+		}
+	}
+	return false
+}
+
+func walkPaths() ([]string, error) {
 	var dirs []string
+
+	err := filepath.Walk("/home/martenvd", func(path string, info os.FileInfo, err error) error {
+		if info != nil && info.IsDir() {
+			output, err := exec.Command("ls", "-a", path).Output()
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+
+			git, err := regexp.MatchString(`\B\.git(\b|$|\s)`, string(output))
+			if err != nil {
+				fmt.Println(err)
+			}
+			if git {
+				dirs = append(dirs, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return dirs, err
+}
+
+func prompt(dirs []string) {
+
+	var relevantDirs []string
 
 	// Get the current directory
 	currentDir, err := os.Getwd()
@@ -27,33 +103,26 @@ func main() {
 		fmt.Println("Please provide a root directory for the search.")
 		return
 	}
-	err = filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
-		if info != nil && info.IsDir() && strings.Contains(strings.ToLower(path), strings.ToLower(os.Args[1])) {
-			output, err := exec.Command("ls", "-a", path).Output()
-			if err != nil {
-				fmt.Println(err)
-				return err
-			}
 
-			git, err := regexp.MatchString("\\.git\\s", string(output))
-			if err != nil {
-				fmt.Println(err)
-			}
-			if git {
-				dirs = append(dirs, path)
-			}
+	arg := 1
+	if isFlag() {
+		if len(os.Args) > 2 {
+			arg = 2
+		} else {
+			fmt.Println("You have updated your cache!")
+			return
 		}
-		return nil
-	})
+	}
 
-	if err != nil {
-		fmt.Println(err)
-		return
+	for _, dir := range dirs {
+		if strings.Contains(dir, currentDir) && strings.Contains(strings.ToLower(dir), strings.ToLower(os.Args[arg])) {
+			relevantDirs = append(relevantDirs, dir)
+		}
 	}
 
 	prompt := promptui.Select{
 		Label: "Select Directory",
-		Items: dirs,
+		Items: relevantDirs,
 	}
 
 	_, result, err := prompt.Run()
@@ -73,4 +142,54 @@ func main() {
 		fmt.Println(err)
 	}
 
+}
+
+func cacheDirs(homeDir string, dirs []string) {
+	// Create a file
+	os.MkdirAll(homeDir+"/.gofind", os.ModePerm)
+	file, err := os.Create(homeDir + "/.gofind/dirs.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Write strings to the file
+	for _, dir := range dirs {
+		_, err := file.WriteString(dir + "\n")
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+			return
+		}
+	}
+}
+
+func readDirs(homeDir string) ([]string, error) {
+	// Open the file for reading
+	file, err := os.Open(homeDir + "/.gofind/dirs.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	// Scanner to read from file
+	scanner := bufio.NewScanner(file)
+
+	// Set to store strings
+	var dirs []string
+
+	// Read strings from the file
+	for scanner.Scan() {
+		dir := scanner.Text()
+		dirs = append(dirs, dir)
+	}
+
+	// Check for scanner errors
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, err
+	}
+
+	return dirs, nil
 }
